5fe2bac83e9cbc679da57ba025f6089ad3bcf307
Hudan Widzamil <hudanwidzamil@gmail.com>
2021-05-27 21:14:22 +0700

    reduce scope

--- 2_C++ OOP/3_ROS Imitation/ServoSubscriber.cpp
+++
@@ -1,7 +0,0 @@
-#include "ServoSubscriber.hpp"
-
-ServoSubscriber::ServoSubscriber(string topic) : Subscriber(topic) {}
-
-void ServoSubscriber::handleMessage(Message msg) {
-  cout << "[id : " << this->id << "] Servo data : " << msg.getData() << endl;
-}
--- 2_C++ OOP/3_ROS Imitation/Subscriber.cpp
+++
@@ -1,9 +0,0 @@
-#include "Subscriber.hpp"
-#include "Master.hpp"
-
-int Subscriber::count = 0;
-
-Subscriber::Subscriber(string topic) {
-  Master::getInstance()->addSubscriber(topic, this);
-  this->id = ++count;
-}
